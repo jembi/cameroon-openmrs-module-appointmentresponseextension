8561ea56e935f4c0971e393406647dc81f19f67e
Bailly RURANGIRWA <rubailly@gmail.com>
2019-06-28 00:51:49 +0200

    COM-393: Removing un-used api classes

--- api/src/test/java/org/openmrs/module/cameroonappointmentresponseextension/CameroonAppointmentResponseExtensionServiceTest.java
+++ api/src/test/java/org/openmrs/module/cameroonappointmentresponseextension/CameroonAppointmentResponseExtensionServiceTest.java
@@ -9,20 +9,6 @@
  */
 package org.openmrs.module.cameroonappointmentresponseextension;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.openmrs.User;
-import org.openmrs.api.UserService;
-import org.openmrs.module.cameroonappointmentresponseextension.Item;
-import org.openmrs.module.cameroonappointmentresponseextension.api.dao.CameroonAppointmentResponseExtensionDao;
-import org.openmrs.module.cameroonappointmentresponseextension.api.impl.CameroonAppointmentResponseExtensionServiceImpl;
-import static org.mockito.Mockito.*;
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
-
 /**
  * This is a unit test, which verifies logic in CameroonAppointmentResponseExtensionService. It
  * doesn't extend BaseModuleContextSensitiveTest, thus it is run without the in-memory DB and Spring
@@ -30,35 +16,4 @@ import static org.junit.Assert.*;
  */
 public class CameroonAppointmentResponseExtensionServiceTest {
 	
-	@InjectMocks
-	CameroonAppointmentResponseExtensionServiceImpl basicModuleService;
-	
-	@Mock
-	CameroonAppointmentResponseExtensionDao dao;
-	
-	@Mock
-	UserService userService;
-	
-	@Before
-	public void setupMocks() {
-		MockitoAnnotations.initMocks(this);
-	}
-	
-	@Test
-	public void saveItem_shouldSetOwnerIfNotSet() {
-		//Given
-		Item item = new Item();
-		item.setDescription("some description");
-		
-		when(dao.saveItem(item)).thenReturn(item);
-		
-		User user = new User();
-		when(userService.getUser(1)).thenReturn(user);
-		
-		//When
-		basicModuleService.saveItem(item);
-		
-		//Then
-		assertThat(item, hasProperty("owner", is(user)));
-	}
 }
